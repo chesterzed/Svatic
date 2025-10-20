e425a43c9a014acb8279aa5f735ef9913042535a
Ratibor Vanin <chester.1.80@mail.ru>
2025-10-20 18:06:42 +0300

    commit

--- svyatic.py
+++ svyatic.py
@@ -4,6 +4,17 @@ import json
 import telebot
 
 BOT_TOKEN = '6817890701:AAFvaqFYn5T_YvF367VVwRDsqIwLV9BbYkE'
+# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# И используйте сессию с отключенной проверкой SSL
+session = requests.Session()
+session.verify = False
+response = session.get('https://api.quotable.io/random', timeout=10)
+print(response.json())
+
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -13,21 +24,19 @@ bul = ["Буль.", "Буль!", "Буль... Буль?", "Буль. Буль. �
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     if "святик" in str(message.text).lower():
-        bot.reply_to(message, bul[random.randint(0, len(bul)-1)])
-    if "святослав" in str(message.text).lower():
-        data = 'Буль'
+        bot.reply_to(message, random.choice(bul))
+    elif "святослав" in str(message.text).lower():
         try:
-            response = requests.get('https://api.quotable.io/random')
+            response = requests.get('https://api.quotable.io/random', timeout=10)
+            print(response)
             if response.status_code == 200:
                 data = response.json()
-                data = f"{data['content']}\n\t\t- {data['author']}"
+                quote_text = f"{data['content']}\n\t\t- {data['author']}"
+                bot.reply_to(message, quote_text)
         except:
-            data = bul[random.randint(0, len(bul) - 1)]
-
-
-        bot.reply_to(message, data)
-
+            data = random.choice(bul)
+            bot.reply_to(message, data)
 
 
 print(random.randint(0, len(bul) - 1))
-bot.infinity_polling()
+bot.infinity_polling(timeout=60)
