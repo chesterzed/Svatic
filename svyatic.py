11d3fe802841ab3c3521c8589248e96c0e022951
Ratibor Vanin <chester.1.80@mail.ru>
2025-10-20 17:38:04 +0300

    святослав

--- svyatic.py
+++ svyatic.py
@@ -1,5 +1,6 @@
 import random
-
+import requests
+import json
 import telebot
 
 BOT_TOKEN = '6817890701:AAFvaqFYn5T_YvF367VVwRDsqIwLV9BbYkE'
@@ -13,6 +14,19 @@ bul = ["Буль.", "Буль!", "Буль... Буль?", "Буль. Буль. �
 def echo_all(message):
     if "святик" in str(message.text).lower():
         bot.reply_to(message, bul[random.randint(0, len(bul)-1)])
+    if "святослав" in str(message.text).lower():
+        data = 'Буль'
+        try:
+            response = requests.get('https://api.quotable.io/random')
+            if response.status_code == 200:
+                data = response.json()
+                data = f"{data['content']}\n\t\t- {data['author']}"
+        except:
+            data = bul[random.randint(0, len(bul) - 1)]
+
+
+        bot.reply_to(message, data)
+
 
 
 print(random.randint(0, len(bul) - 1))
